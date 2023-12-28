ea39132a7ca604b141ecc1388d0160b5bd669048
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2023-12-27 19:21:08 -0500

    finished title page

---
+++ portfolio/src/Components/Header/index.js
@@ -0,0 +1,17 @@
+import './header.css';
+
+
+export const Header = () =>{
+
+    return (
+        <header className= "something">
+            <h5>About</h5>
+            <h5>Projects</h5>
+            <h5>Experiance</h5>
+        </header>
+    )
+
+
+
+
+}
---
+++ portfolio/src/pages/Home/index.js
@@ -0,0 +1,29 @@
+
+import React from "react"
+import { useTypewriter } from 'react-simple-typewriter'
+import './home.css';
+
+
+
+export default function Home() {
+
+
+  const [text, speed] = useTypewriter({
+    words: [
+      "Hi, I'm Ahmad",
+      "I'm a Full Stack Developer",
+      "Coding dreams into reality, end to end.",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
+
+  return (
+    <div className='container'>
+      <h1>{text}</h1>
+    </div>
+
+  )
+
+}
+
