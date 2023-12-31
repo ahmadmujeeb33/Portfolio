7e59b980ceb8952965fa377c6274bfed67b2efa9
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2023-12-30 22:45:42 -0500

    updated header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -7,9 +7,9 @@ export const Header = () =>{
 
   return (
       <header className = 'navbar-items'>
-          <Link to ="/about" className="items">About</Link>
-          <Link to ="/projects" className="items">Projects</Link>
-          <Link to ="/experiance" className="items">Experiance</Link>
+          <Link to ="/portfolio/about" className="items">About</Link>
+          <Link to ="/portfolio/projects" className="items">Projects</Link>
+          <Link to ="/portfolio/experiance" className="items">Experiance</Link>
       </header>
   )
 
