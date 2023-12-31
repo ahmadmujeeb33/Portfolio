f0e3c9bbfe78c5e9e521257fe4bf6d3cb610ec1e
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2023-12-30 23:13:32 -0500

    adjusted header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -4,16 +4,11 @@ import { Link } from "react-router-dom"
 
 
 export const Header = () =>{
-
   return (
-      <header className = 'navbar-items'>
-          <Link to ="/portfolio/about" className="items">About</Link>
-          <Link to ="/portfolio/projects" className="items">Projects</Link>
-          <Link to ="/portfolio/experiance" className="items">Experiance</Link>
-      </header>
+    <header className = 'navbar-items'>
+        <Link to ="/portfolio/about" className="items">About</Link>
+        <Link to ="/portfolio/projects" className="items">Projects</Link>
+        <Link to ="/portfolio/experiance" className="items">Experiance</Link>
+    </header>
   )
-
-
-
-
 }
