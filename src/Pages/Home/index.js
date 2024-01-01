9ab094c5853c940a6c733ec1e1a7bb225419fbcc
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2024-01-01 16:21:58 -0500

    fixed spacing

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -3,8 +3,6 @@ import React from "react"
 import { useTypewriter } from 'react-simple-typewriter'
 import './home.css';
 
-
-
 export const Home = () => {
   const [text, speed] = useTypewriter({
     words: [
